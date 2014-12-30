409a8070cd1012a31269355d75ecaf5426947144
awestover <awestover@ifbyphone.com>
2014-12-30 14:01:58 -0600

    Switching to use Zone_Changed_New_Area event

--- InstanceProfits.lua
+++ InstanceProfits.lua
@@ -1,9 +1,13 @@
+local inInstance = false;
+
 function InstanceProfits_EventHandler(self, event, ...)
-  if (event == "RAID_INSTANCE_WELCOME") then
-  	local name, time_left = ...;
-  	print("You are entering " .. name .. " and your instance lock will expire in " .. time_left);
-  end
-  if (event == "PLAYER_LEAVING_WORLD") then
-  	print("You are leaving.");
+  if (event == "ZONE_CHANGED_NEW_AREA") then
+  	local name, type, difficulty, difficultyName = GetInstanceInfo();
+  	if (type == "raid" || type == "party") then
+  		inInstance = true;
+  		print("You have entered the " .. difficultyName .. " version of " .. name);
+  	elseif inInstance then
+  		inInstance = false;
+  		print("You have exited your instance.")
   end
 end
