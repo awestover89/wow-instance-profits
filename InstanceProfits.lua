edce9c579dc807270c95689913a983013c5f43fe
awestover <awestover@ifbyphone.com>
2014-12-30 14:21:18 -0600

    Adding timer functionality

--- InstanceProfits.lua
+++ InstanceProfits.lua
@@ -1,13 +1,16 @@
 local inInstance = false;
+local startTime = 0;
 
 function InstanceProfits_EventHandler(self, event, ...)
   if (event == "ZONE_CHANGED_NEW_AREA") then
   	local name, type, difficulty, difficultyName = GetInstanceInfo();
   	if (type == "raid" || type == "party") then
   		inInstance = true;
+  		startTime = time();
   		print("You have entered the " .. difficultyName .. " version of " .. name);
   	elseif inInstance then
   		inInstance = false;
-  		print("You have exited your instance.")
+  		local totalTime = difftime(time(), startTime);
+  		print("You have exited your instance after spending " .. totalTime .. " seconds inside.");
   end
 end
